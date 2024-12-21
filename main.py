9eafefdd223c0baf5b5b24a32fcb15a84d5ae837
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2024-12-21 14:39:37 +0100

    Some routes

--- main.py
+++ main.py
@@ -10,5 +10,10 @@ class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
 
+@time_calculous_api.route('/hello_1/<id>/<id_1>', doc={'params': {'id': 'An ID', 'id_1': 'An ID_1'}})
+class HelloWorld_1(Resource):
+    def get(self,id,id_1):
+        return {'nbr': id}
+
 if __name__ == '__main__':
     time_calculous_with_flask_restx.run(debug=True)
