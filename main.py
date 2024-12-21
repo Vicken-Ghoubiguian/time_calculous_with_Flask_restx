d9b3d60d1a2d8800eb66924e2da153553aa4708a
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2024-12-21 15:58:46 +0100

    For the future

--- main.py
+++ main.py
@@ -6,15 +6,10 @@ time_calculous_api = Api(time_calculous_with_flask_restx, title='time_calculous 
 
 ns_time_calculous = time_calculous_api.namespace('time_calculous_operations', description='time_calculous operations for')
 
-@ns_time_calculous.route('/')
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
-
-@ns_time_calculous.route('/hello_1/<int:id>/<int:id_1>', doc={'params': {'id': 'An ID', 'id_1': 'An ID_1'}})
+@ns_time_calculous.route('/<string:operation>/<int:id>/<int:id_1>', doc={'params': {'id': 'An ID', 'id_1': 'An ID_1'}})
 class HelloWorld_1(Resource):
-    def get(self,id,id_1):
-        return {'nbr': id, 'nbr_1': id_1}
+    def get(self,operation,id,id_1):
+        return {'operation': operation, 'nbr': id, 'nbr_1': id_1}
 
 if __name__ == '__main__':
     time_calculous_with_flask_restx.run(debug=True)
