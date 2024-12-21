b1b43bbd42d3a788b47b0ea9d2ad6ed0b73de058
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2024-12-21 15:43:52 +0100

    Some additions

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ from flask_restx import Resource, Api
 time_calculous_with_flask_restx = Flask(__name__)
 time_calculous_api = Api(time_calculous_with_flask_restx, title='time_calculous API', description='Coming !')
 
-@time_calculous_api.route('/hello')
+ns_time_calculous = time_calculous_api.namespace('time_calculous_operations', description='time_calculous operations for')
+
+@ns_time_calculous.route('/')
 class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
 
-@time_calculous_api.route('/hello_1/<int:id>/<int:id_1>', doc={'params': {'id': 'An ID', 'id_1': 'An ID_1'}})
+@ns_time_calculous.route('/hello_1/<int:id>/<int:id_1>', doc={'params': {'id': 'An ID', 'id_1': 'An ID_1'}})
 class HelloWorld_1(Resource):
     def get(self,id,id_1):
         return {'nbr': id, 'nbr_1': id_1}
