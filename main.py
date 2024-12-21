71d833062be80df86d6bbf22262ae5faa8d580e6
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2024-12-21 16:10:55 +0100

    Word of today's end

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ time_calculous_api = Api(time_calculous_with_flask_restx, title='time_calculous 
 
 ns_time_calculous = time_calculous_api.namespace('time_calculous_operations', description='time_calculous operations for')
 
-@ns_time_calculous.route('/<string:function>/<int:id>/<int:id_1>', doc={'params': {'function': 'function of the "time_calculous" C library to execute', 'id': 'An ID', 'id_1': 'An ID_1'}})
-class HelloWorld_1(Resource):
+@ns_time_calculous.route('/<string:function>/<int:id>/<int:id_1>', doc={'params': {'function': 'Choosed function of the "time_calculous" C library to execute', 'id': 'An ID', 'id_1': 'An ID_1'}})
+class Time_calculous_main_route(Resource):
     def get(self,function,id,id_1):
         return {'function': function, 'nbr': id, 'nbr_1': id_1}
 
