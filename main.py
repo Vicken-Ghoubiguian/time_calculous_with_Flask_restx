46c4feafbac32cf37f3d355b5b0a860e3372ad8a
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2024-12-22 09:51:46 +0100

    For the future routes' description

--- main.py
+++ main.py
@@ -9,12 +9,22 @@ ns_time_calculous = time_calculous_api.namespace('time_calculous_operations', de
 @ns_time_calculous.route('')
 class Time_calculous_presentation_route(Resource):
     def get(self):
+
+        """
+        Route 1, coming !
+        """
+
         return {'presentation' : '', 'description': '', 'github' : 'https://github.com/Vicken-Ghoubiguian/time_calculous_with_Flask_restx', 'dockerhub' : '', 'time_calculous' : 
 'https://github.com/Vicken-Ghoubiguian/time_calculous'}, 200
 
 @ns_time_calculous.route('/<string:function>/<int:id>/<int:id_1>', doc={'params': {'function': 'Choosed function of the "time_calculous" C library to execute', 'id': 'An ID', 'id_1': 'An ID_1'}})
 class Time_calculous_main_route(Resource):
     def get(self,function,id,id_1):
+
+        """
+        Route 2, coming !
+        """
+
         return {'function': function, 'nbr': id, 'nbr_1': id_1}, 200
 
 if __name__ == '__main__':
