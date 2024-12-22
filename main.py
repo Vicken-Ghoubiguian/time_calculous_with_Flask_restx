b34c83633cee096504f90a7caf64f8c105e67464
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2024-12-22 09:44:13 +0100

    New route

--- main.py
+++ main.py
@@ -6,10 +6,16 @@ time_calculous_api = Api(time_calculous_with_flask_restx, title='time_calculous 
 
 ns_time_calculous = time_calculous_api.namespace('time_calculous_operations', description='time_calculous operations for')
 
+@ns_time_calculous.route('')
+class Time_calculous_presentation_route(Resource):
+    def get(self):
+        return {'presentation' : '', 'description': '', 'github' : 'https://github.com/Vicken-Ghoubiguian/time_calculous_with_Flask_restx', 'dockerhub' : '', 'time_calculous' : 
+'https://github.com/Vicken-Ghoubiguian/time_calculous'}, 200
+
 @ns_time_calculous.route('/<string:function>/<int:id>/<int:id_1>', doc={'params': {'function': 'Choosed function of the "time_calculous" C library to execute', 'id': 'An ID', 'id_1': 'An ID_1'}})
 class Time_calculous_main_route(Resource):
     def get(self,function,id,id_1):
-        return {'function': function, 'nbr': id, 'nbr_1': id_1}
+        return {'function': function, 'nbr': id, 'nbr_1': id_1}, 200
 
 if __name__ == '__main__':
     time_calculous_with_flask_restx.run(debug=True)
