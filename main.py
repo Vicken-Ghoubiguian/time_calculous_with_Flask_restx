caaae6f90676afca1bd78bed620862ccb6ca3896
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2025-01-10 17:55:52 +0100

    New route

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from flask import Flask
 from flask_restx import Resource, Api
 from ctypes import *
+#from enum import numeral
 from os import path, system
 
 #
@@ -16,6 +17,13 @@ if not path.exists(so_time_calculous_file):
 
 #
 time_calculous_functions = CDLL(so_time_calculous_file)
+
+#class NUMERAL(numeral):
+#    FIRST = 0
+#    SECOND = 1
+#    THIRD = 2
+#    BEFORE_LAST = 3
+#    LAST = 4
 
 # Definition of the 'time_calculous_function' array which contains all functions in the 'time_calculous' C library
 time_calculous_function = ['number_of_days_in_choosen_month_in_choosen_year', 'wished_number_in_year_is_day_in_choosen_year', 'wished_wday_in_choosen_year']
@@ -122,6 +130,20 @@ class Time_calculous_functions_wishedNumberInYearIsDayInChoosenYear(Resource):
 
         #
         return {'mday' : mday, 'month' : month, 'year' : year, 'result' : result}, 200
+    
+#
+@ns_time_calculous_functions.route('/wished_wday_in_choosen_month/<int:year>/<int:month>/<int:wday>/<int:hour>/<int:minute>/<int:second>', doc={})
+class Time_calculous_functions_wishedWdayInChoosenMonth(Resource):
+    def get(self, year, month, wday, hour, minute, second):
+
+        """
+        """
+
+        #
+        result = time_calculous_functions.wished_wday_in_choosen_month(year, month, wday, hour, minute, second, 0)
+
+        #
+        return {'result' : result}
 
 #
 if __name__ == '__main__':
